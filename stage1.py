4861ad059aaf5f4108179c4c3b0e70aae566d98f
AshwinNBhanushali <ashwin.bhanushali@atritechnocrat.com>
2019-11-17 00:17:05 +0530

    stage 3

--- stage1.py
+++ stage1.py
@@ -1,20 +1,33 @@
 print("Welcome to PiggyBank")
 
-def deposit(amount):
-  global balance
-  global lt
-  balance = balance + amount
-  lt = amount
-
 balance = 0
 print("Balance =",balance)
 
 lt = 0
 print("Last Transaction =",balance)
 
+def deposit(amount):
+  global balance
+  global lt
+  if(amount > 0):
+      balance = balance + amount
+      lt = amount
+
+def withdraw(amount):
+  global balance
+  global lt
+  if(balance >= amount):
+      balance = balance - amount
+      lt = -amount
+
 
 deposit(50)
 deposit(100)
+
+print("Balance =",balance)
+print("Last Transaction =",lt)
+
+withdraw(70)
 
 print("Balance =",balance)
 print("Last Transaction =",lt)
